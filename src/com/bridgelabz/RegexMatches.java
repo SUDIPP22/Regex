06d80a2e0263585084123b6a88768d02eb8f4cbd
SUDIPP22 <panja370@gmail.com>
2021-09-12 18:27:59 +0530

    Refactored : To validate Sample E-Mail IDs'

--- src/com/bridgelabz/RegexMatches.java
+++ src/com/bridgelabz/RegexMatches.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 /**
  * Purpose :To implement usage of Java Regular Expression and Patterns
  * To validate the First Name, Last Name, Email-ID, Mobile Number, Password of user using regex
+ * To validate sample mails
  * @author SUDIP PANJA
  * @version 11.0.11
  * @since 2021-09-09
@@ -12,5 +13,6 @@ public class RegexMatches {
         System.out.println("Welcome To The Program");
         UserRegistration userRegistration = new UserRegistration();
         userRegistration.validateUserDetails();
+        SampleEmails.sampleEmailCheck();
     }
 }
